7d0644b2fc65301e4c119f520a66a472ee5a13e9
KyanCh <zgy.wiw@outlook.com>
2022-06-14 20:46:04 +0800

    dotenv

--- file_manager/tests/sqlx_test.rs
+++ file_manager/tests/sqlx_test.rs
@@ -1,21 +1,25 @@
-use sqlx::postgres::PgPoolOptions;
+use std::env;
 
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 #[test]
 fn getx() {
+    dotenv().ok();
+    let pgurl=env::var("PGURL").unwrap();
     println!("Hello, Sqlx!");
     // tokio runtime
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all().build().unwrap();
     runtime.block_on( async {
-        connect()
+        connect(&pgurl)
             .await.unwrap();
     });
 }
-async fn connect()->Result<(), sqlx::Error>{
+async fn connect(pgurl: &str)->Result<(), sqlx::Error>{
     let pool =PgPoolOptions::new()
         .max_connections(5)
-        .connect(r#"postgresql://file:filefile@sh-tdcpg-ep-24rz9f8y.sql.tencentcdb.com:28867/file"#).await?;
+        .connect(pgurl).await?;
     sqlx::query(r#"CREATE TABLE IF NOT EXISTS person (
             id bigserial,
             name varchar NOT NULL
@@ -24,5 +28,3 @@ async fn connect()->Result<(), sqlx::Error>{
         .await?;
     Ok(())
 }
-
-
